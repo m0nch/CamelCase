4cfc2e16cef7aed61eacae63a91d34bf082f7344
m0nch <margarita.davtyan@gmail.com>
2021-12-10 00:53:16 +0400

    Add project files.

---
+++ CamelCase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Result
+{
+    public static int Camelcase(string s)
+    {
+        int wordCount = 1;
+        char[] charArray = s.ToCharArray();
+        foreach (var item in charArray)
+        {
+            if (char.IsUpper(item))
+            {
+                wordCount++;
+            }
+        }
+        return wordCount;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        int result = Result.Camelcase(s);
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
+
